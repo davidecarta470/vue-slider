2ee7d61a54c81eeb7106ede8ac7e515dcd4bdfff
davidecarta470 <cartadavi@gmail.com>
2021-11-11 17:45:27 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -5,12 +5,16 @@ const app = new Vue({
   
   data: {
     images : ['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'],
-    counter: 0
+    counter: 0,
+    precedente : 'prev',
+    successiva : 'next'
+
   },
   mounted(){
     setInterval(() => {
       this.nextSlide();
-    }, 3000)
+    },3000)
+
   },
   methods:{
 
