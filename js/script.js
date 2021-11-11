94acaf7089f7618291c1be6d8a4d0c7cb4a7ba6a
davidecarta470 <cartadavi@gmail.com>
2021-11-11 16:49:13 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+const app = new Vue({
+  el: '#app',
+  
+  data: {
+    images : ['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'],
+    counter: 0
+  },
+  mounted(){
+    setInterval(() => {
+      this.nextSlide();
+    }, 3000)
+  },
+  methods:{
+
+    nextSlide(){
+      this.counter++;
+      if(this.counter > this.images.length - 1){
+        this.counter = 0;
+      }
+    },
+
+    prevSlide(){
+      this.counter--;
+      if(this.counter < 0){
+        this.counter = this.images.length - 1;
+      }
+    }
+
+  }
+
+})
